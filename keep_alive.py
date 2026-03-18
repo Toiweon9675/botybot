153f72d632729aaf0c71a20bb808f3a272f51ab6
Isaac Leal <rociojurado3009@gmail.com>
2026-03-18 20:54:58 +0100

    Update Flask app response and port configuration

--- keep_alive.py
+++ keep_alive.py
@@ -1,14 +1,15 @@
 from flask import Flask
 from threading import Thread
+import os
 
 app = Flask('')
 
 @app.route('/')
-def index():
-    return "Bot activo"
+def home():
+    return "Ramona bot vive 😈"
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 10000)))
 
 def keep_alive():
     t = Thread(target=run)
